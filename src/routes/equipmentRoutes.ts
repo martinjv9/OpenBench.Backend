efced214abb50ab59ca7825e89c97c32a3e32385
Martin Jimenez <martinjv9@gmail.com>
2025-04-07 17:45:31 -0700

    Equipment Routes

--- src/routes/equipmentRoutes.ts
+++ src/routes/equipmentRoutes.ts
@@ -1,8 +1,10 @@
 import express from "express";
-import { getEquipmentSummary } from "../controllers/equipmentController";
+import { getEquipmentSummary, getEquipmentActive } from "../controllers/equipmentController";
 
 const router = express.Router();
 
 router.get("/summary", getEquipmentSummary);
+router.get("/active", getEquipmentActive)
+
 
 export default router;
