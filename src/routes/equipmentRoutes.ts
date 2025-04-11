c22d8c28c740693b00b2aad1edd0621088fd2b7e
Martin Jimenez <martinjv9@gmail.com>
2025-04-11 15:24:44 -0700

    OPB-42: Add role-based protection to equipment routes

--- src/routes/equipmentRoutes.ts
+++ src/routes/equipmentRoutes.ts
@@ -1,10 +1,19 @@
 import express from "express";
-import { getEquipmentSummary, getEquipmentActive } from "../controllers/equipmentController";
+import {
+  getEquipmentSummary,
+  getEquipmentActive,
+} from "../controllers/equipmentController";
+import { authenticateToken } from "../middlewares/authMiddleware";
+import { authorizeRoles } from "../middlewares/roleMiddleware";
 
 const router = express.Router();
 
-router.get("/summary", getEquipmentSummary);
-router.get("/active", getEquipmentActive)
-
+router.get(
+  "/summary",
+  authenticateToken,
+  authorizeRoles("user", "technician", "admin"),
+  getEquipmentSummary
+);
+router.get("/active", getEquipmentActive);
 
 export default router;
